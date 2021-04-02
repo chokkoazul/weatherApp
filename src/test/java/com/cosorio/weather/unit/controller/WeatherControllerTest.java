0f370942b8ff6ea4ff84c412ae3d6a11820d53d6
Cristian Osorio <cristian.osorio@globant.com>
2021-04-02 19:27:57 -0300

    se agregan mas test

--- src/test/java/com/cosorio/weather/unit/controller/WeatherControllerTest.java
+++ src/test/java/com/cosorio/weather/unit/controller/WeatherControllerTest.java
@@ -12,10 +12,13 @@ import org.mockito.junit.MockitoJUnitRunner;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -42,11 +45,33 @@ public class WeatherControllerTest {
         List<WeatherDomain> listResponseEntity = weatherController.getAllWeathers(null);
 
 
-        //assertNotNull("weatherDomains.size() should be 1",listResponseEntity.getBody());
         assertNotNull("weatherDomains.size() should be 1",listResponseEntity);
-        //assertEquals("listResponseEntity.getBody() should be equals to [MeteorologicalDomain{id=666, date=2021-01-02, location=null, temperature=null}]", listResponseEntity.getBody().toString(), "[MeteorologicalDomain{id=666, date=2021-01-02, location=null, temperature=null}]");
         assertEquals("listResponseEntity.getBody() should be equals to [MeteorologicalDomain{id=666, date=2021-01-02, location=null, temperature=null}]", listResponseEntity.toString(), "[MeteorologicalDomain{id=666, date=2021-01-02, location=null, temperature=null}]");
 
     }
+
+    @Test
+    public void getAllWeathersByDateResponseOk() {
+        when(weatherService.getWeatherByDate(anyString())).thenReturn(Arrays.asList(WeatherDomain.builder().id(666L).date(LocalDate.of(2021, 1, 2)).build()));
+
+        List<WeatherDomain> listResponseEntity = weatherController.getAllWeathers("2021-1-2");
+
+        assertNotNull("weatherDomains.size() not should be null",listResponseEntity);
+        assertEquals("listResponseEntity.getBody() should be equals to [MeteorologicalDomain{id=666, date=2021-01-02, location=null, temperature=null}]", listResponseEntity.toString(), "[MeteorologicalDomain{id=666, date=2021-01-02, location=null, temperature=null}]");
+
+    }
+
+    @Test
+    public void getWeatherByIdResponseOk() {
+        when(weatherService.getWeather(anyLong())).thenReturn(WeatherDomain.builder().id(666L).build());
+
+        WeatherDomain responseEntity = weatherController.getWeather(1L);
+
+        assertNotNull("responseEntity not should be null",responseEntity);
+
+    }
+
+
+
 
 }
